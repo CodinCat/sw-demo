f2a715e926652b86b2e30bc97c752bf5c1fd1ba1
CodinCat <a55951234@gmail.com>
2017-02-13 18:59:08 +0800

    init

---
+++ sw.js
@@ -0,0 +1,29 @@
+const move = (s, t) => { t.push(s.pop()) }
+
+const hanoi = (source, temp, target, n) => {
+  if (n === 1) { return move(source, target) }
+  hanoi(source, target, temp, n - 1)
+  move(source, target)
+  hanoi(temp, source, target, n - 1)
+}
+
+const createStack = n => [...(new Array(n)).keys()]
+
+const runNewHanoi = n => {
+  const a = createStack(n)
+  const b = []
+  const c = []
+
+  console.log('Starting hanoi', a, b, c)
+  console.time('NonWorker')
+  hanoi(a, b, c, n)
+  console.timeEnd('NonWorker')
+  console.log('Finished', a, b, c)
+}
+
+onmessage = e => {
+  if (e.data.cmd === 'start') {
+    runNewHanoi(e.data.n)
+    postMessage('finished')
+  }
+}
